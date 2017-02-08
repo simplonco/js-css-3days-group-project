4fe6ec343162a4e1b496d3276fd58b94c24a9b50
david <ostermann.david@gmail.com>
2017-02-08 09:20:50 +0100

    add ids

--- data/data.js
+++ data/data.js
@@ -13,18 +13,21 @@ var contenu = {
   ville: 'Montreuil',
   carte: [
     {
+      id: 1,
       name: 'Poulet aux morilles',
       image: 'http://www.epicurien.be/img/recettes-cuisines/20130928_poulet_morilles-500.jpg',
       description: 'Un grand classique le poulet de Bresse aux morilles et à la crème. Si vous ne trouvez pas de volaille de Bresse, choisissez un bon poulet fermier, élevé aux grains et qui a vécu en liberté dans les champs.',
       price: '23 €'
     },
     {
+      id: 2,
       name: 'Tagine aux pruneaux',
       image:'https://i1.wp.com/cotesoleils.fr/wp-content/uploads/2015/06/tajine-agneau-et-pruneaux-algc3a9rie1.jpg',
       description: 'Un des plus célèbres Tajines Marocains, servi avec une délicieuse viande de veau et des pruneaux issus de l\'agriculture biologique',
       price: '18 €'
     },
     {
+      id: 3,
       name: 'Salade de saison',
       image: 'http://scrat.hellocoton.fr/img/guide/recette-ma-salade-tiede-de-chou-kale-et-lentilles-patates-douces-et-saumon-fume-18911644.jpg',
       description: 'Toujours à l\'heure, la salade de saison vous régalera avec ses légumes',
